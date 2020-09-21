325351d851c425dd66809af312538e005904523b
jdhrrs <okstateboy2015@gmail.com>
2020-09-21 16:15:13 +0000

    created program

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+/**
+ * main - prints sizes of different data types
+ * Description: Prints sizes of char, int, long int, long long int, and float
+ * Return: 0
+ */
+int main(void)
+{
+  printf("Size of a char: %u byte(s)\n", sizeof(char));
+  printf("Size of an int: %u byte(s)\n", sizeof(int));
+  printf("Size of a long int: %u byte(s)\n", sizeof(long));
+  printf("Size of a long long int: %u byte(s)\n", sizeof(long long));
+  printf("Size of a float: %u byte(s)\n", sizeof(float));
+  return (0);
+}
+#include <stdio.h>
+/**
+ * main - prints sizes of different data types
+ * Description: Prints sizes of char, int, long int, long long int, and float
+ * Return: 0
+ */
+int main(void)
+{
+  printf("Size of a char: %u byte(s)\n", sizeof(char));
+  printf("Size of an int: %u byte(s)\n", sizeof(int));
+  printf("Size of a long int: %u byte(s)\n", sizeof(long));
+  printf("Size of a long long int: %u byte(s)\n", sizeof(long long));
+  printf("Size of a float: %u byte(s)\n", sizeof(float));
+  return (0);
+}
+#include <stdio.h>
+/**
+ * main - prints sizes of different data types
+ * Description: Prints sizes of char, int, long int, long long int, and float
+ * Return: 0
+ */
+int main(void)
+{
+  printf("Size of a char: %u byte(s)\n", sizeof(char));
+  printf("Size of an int: %u byte(s)\n", sizeof(int));
+  printf("Size of a long int: %u byte(s)\n", sizeof(long));
+  printf("Size of a long long int: %u byte(s)\n", sizeof(long long));
+  printf("Size of a float: %u byte(s)\n", sizeof(float));
+  return (0);
+}
+#include <stdio.h>
+/**
+ * main - prints sizes of different data types
+ * Description: Prints sizes of char, int, long int, long long int, and float
+ * Return: 0
+ */
+int main(void)
+{
+  printf("Size of a char: %u byte(s)\n", sizeof(char));
+  printf("Size of an int: %u byte(s)\n", sizeof(int));
+  printf("Size of a long int: %u byte(s)\n", sizeof(long));
+  printf("Size of a long long int: %u byte(s)\n", sizeof(long long));
+  printf("Size of a float: %u byte(s)\n", sizeof(float));
+  return (0);
+}
