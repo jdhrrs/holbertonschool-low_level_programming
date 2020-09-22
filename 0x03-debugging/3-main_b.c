89abc8494b3ff66b613d527da276ac10d54dd95a
jdhrrs <51782837+jdhrrs@users.noreply.github.com>
2020-09-22 16:24:40 -0500

    Add files via upload

---
+++ 0x03-debugging/3-main_b.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - takes a date and prints how many days are left in the year, taking
+ * leap years into account
+ * Return: 0
+ */
+
+int main(void)
+{
+	int month;
+	int day;
+	int year;
+
+	month = 2;
+	day = 28;
+	year = 2000;
+	printf("Date: %02d/%02d/%04d\n", month, day, year);
+	day = convert_day(month, day);
+	print_remaining_days(month, day, year);
+
+	month = 2;
+	day = 29;
+	year = 2000;
+	printf("Date: %02d/%02d/%04d\n", month, day, year);
+	day = convert_day(month, day);
+	print_remaining_days(month, day, year);
+	month = 2;
+	day = 28;
+
+	month = 3;
+	day = 1;
+	year = 2000;
+	printf("Date: %02d/%02d/%04d\n", month, day, year);
+	day = convert_day(month, day);
+	print_remaining_days(month, day, year);
+
+	month = 2;
+	day = 29;
+	year = 2100;
+	printf("Date: %02d/%02d/%04d\n", month, day, year);
+	day = convert_day(month, day);
+	print_remaining_days(month, day, year);
+	return (0);
+}
