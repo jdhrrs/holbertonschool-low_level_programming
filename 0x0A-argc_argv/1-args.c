7319c5b6f1d8e524d5335e1ceee4e39c9efdc5a5
Justin Harris <okstateboy2015@gmail.com>
2021-02-27 20:27:13 +0000

    Created program that should print a number followed by a new line

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - function
+ * @argc: number of arguments
+ * @argv: array of strings
+ * Return: 0
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+printf("%d\n", argc - 1);
+return (0);
+}
