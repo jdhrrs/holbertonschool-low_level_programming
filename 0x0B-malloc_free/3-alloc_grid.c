97dd7b64898b072886b3e225711619cd3c04c225
Justin Harris <okstateboy2015@gmail.com>
2021-03-02 21:02:16 +0000

    Created a function that returns a pointer to a 2D array of integers.

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,36 +1,50 @@
+#include <stdlib.h>
 #include "holberton.h"
-
 /**
- * alloc_grid - create a gid of numbers
- * @width: the width of grid
- * @height: the height f the grid
- *
- * Return: A pointer to the grid.
+ * alloc_grid - 2 dimensional array of int
+ * @width: columns
+ * @height: rows
+ * Return: s or NULL
  */
 int **alloc_grid(int width, int height)
 {
-int i, j, **h;
+  int i, j;
+  int **s;
 
-if (width <= 0 || height <= 0)
-return (NULL);
+  if (width <= 0 || height <= 0)
+    {
+      return (NULL);
+    }
 
-h = (int **) malloc(height * sizeof(long));
-if (h == NULL)
-return (NULL);
-for (i = 0; i < height; i++)
-{
-*(h + i) = malloc(width * sizeof(int));
-if (*(h + i) == NULL)
-{
-int k;
+  s = malloc(sizeof(int *) * height);
+  if (s == NULL)
+    {
+      free(s);
+      return (NULL);
+    }
 
-for (k = i; k >= 0; k--)
-free(*(h + k));
-free(h);
-return (NULL);
+  for (i = 0; i < height; i++)
+    {
+      s[i] = malloc(sizeof(int) * width);
+      if (s[i] == NULL)
+	{
+	  for (i--; i >= 0; i--)
+	    {
+	      free(s[i]);
+	    }
+	  free(s);
+	  return (NULL);
+	}
+    }
+
+  for (i = 0; i < height; i++)
+    {
+      for (j = 0; j < width; j++)
+	{
+	  s[i][j] = 0;
+	}
+    }
+
+  return (s);
 }
-for (j = 0; j < width; j++)
-h[i][j] = 0;
-}
-return (h);
-}
+
