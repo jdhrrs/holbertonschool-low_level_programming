72df2c8b5f9f29df331eb00520c6d2bf0473503c
Justin Harris <okstateboy2015@gmail.com>
2021-03-02 21:16:01 +0000

    Created a function that concatenates all the arguments of your program

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -9,42 +9,42 @@
  */
 char *argstostr(int ac, char **av)
 {
-  int i, j, size, k;
-  char *s;
+int i, j, size, k;
+char *s;
 
-  if (ac == 0 || av == NULL)
-    {
-      return (NULL);
-    }
+if (ac == 0 || av == NULL)
+{
+return (NULL);
+}
 
-  k = 0;
-  size = 0;
+k = 0;
+size = 0;
 
-  for (i = 0; i < ac; i++)
-    {
-      for (j = 0; av[i][j] != '\0'; j++)
-	{
-	  k++;
-	}
-    }
-  size = ac + k;
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j] != '\0'; j++)
+{
+k++;
+}
+}
+size = ac + k;
 
-  s = (char *)malloc(sizeof(char) * (size + 1));
-  if (s == NULL)
-    {
-      return (NULL);
-    }
+s = (char *)malloc(sizeof(char) * (size + 1));
+if (s == NULL)
+{
+return (NULL);
+}
 
-  k = 0;
+k = 0;
 
-  for (i = 0; i < ac; i++)
-    {
-      for (j = 0; av[i][j] != '\0'; j++, k++)
-	{
-	  s[k] = av[i][j];
-	  s[k + 1] = '\0';
-	}
-      s[k++] = '\n';
-    }
-  return (s);
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j] != '\0'; j++, k++)
+{
+s[k] = av[i][j];
+s[k + 1] = '\0';
+}
+s[k++] = '\n';
+}
+return (s);
 }
