f3c8bd4ad9913c357a349a0bcc544edbbb905e6b
Justin Harris <okstateboy2015@gmail.com>
2021-03-04 16:38:08 +0000

    Created header file for holberton tasks in more malloc

---
+++ 0x0C-more_malloc_free/holberton.h
@@ -0,0 +1,13 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+void *malloc_checked(unsigned int b);
+int _putchar(char c);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int isnum(char *s);
+int _putchar(char c);
+
+#endif
