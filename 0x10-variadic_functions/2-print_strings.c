c57c29c420369c5ddfb886822ba390cf250214a2
Justin Harris <okstateboy2015@gmail.com>
2021-03-11 20:32:15 +0000

    Made betty compliant

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,19 +10,19 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-  va_list valist;
-  unsigned int i;
-  char *p;
+va_list valist;
+unsigned int i;
+char *p;
 
-  va_start(valist, n);
+va_start(valist, n);
 
-  for (i = 0; i < n; i++)
-    {
-      if (separator != NULL && i != 0)
-	printf("%s", separator);
-      p = va_arg(valist, char *);
-      printf("%s", (p == NULL) ? "(nil)" : p);
-    }
-  printf("\n");
-  va_end(valist);
+for (i = 0; i < n; i++)
+{
+if (separator != NULL && i != 0)
+printf("%s", separator);
+p = va_arg(valist, char *);
+printf("%s", (p == NULL) ? "(nil)" : p);
+}
+printf("\n");
+va_end(valist);
 }
