6f845ab46567ed90fe29fc25d54e0eece6c911c0
Justin Harris <okstateboy2015@gmail.com>
2021-03-22 17:30:36 +0000

    Made betty compliant

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -7,20 +7,20 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-  listint_t *new;
+listint_t *new;
 
-  new = malloc(sizeof(listint_t));
-  if (new == NULL)
-    {
-      return (NULL);
-    }
-  if (new)
-    {
-      new->n = n;
-      new->next = *head;
-      *head = new;
-      return (new);
-    }
-  free(new);
-  return (NULL);
+new = malloc(sizeof(listint_t));
+if (new == NULL)
+{
+return (NULL);
+}
+if (new)
+{
+new->n = n;
+new->next = *head;
+*head = new;
+return (new);
+}
+free(new);
+return (NULL);
 }
